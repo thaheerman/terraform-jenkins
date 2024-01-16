26cceb70a8f9b5556f866c6e50a780479881180d
thaheerman <145525025+thaheerman@users.noreply.github.com>
2024-01-17 00:16:22 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "naresh-aws-test"
     key = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "DynamoDbLock"
   }
 }
