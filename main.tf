1dcc7effc621510bc164cfaf34a02ff6b15945b8
thaheerman <145525025+thaheerman@users.noreply.github.com>
2024-01-17 10:01:39 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "jenkins-sg-2022" {
 
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
-## key_name = var.key_name
+ #key_name = var.key_name
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
   tags= {
